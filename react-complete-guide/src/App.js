3ad4a46cd617f5c74f197cd1379992df399b73bb
Priyansh Sangule <psangule@schoology.com>
2021-04-11 18:33:25 +0530

    Person

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Person  from './Person/Person';
 function App() {
   return (
     <div className="App">
       <h1> First App huhu </h1>
+      <Person />
     </div>
   );
 }
