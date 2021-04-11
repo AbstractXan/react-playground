b2b396d7038edd9e59766e5ab9987167b73153b1
Priyansh Sangule <psangule@schoology.com>
2021-04-11 18:46:35 +0530

    State

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Person  from './Person/Person';
-function App() {
-  return (
-    <div className="App">
-      <h1> First App huhu </h1>
-      <Person />
-    </div>
-  );
+import { Component } from 'react';
+class App extends Component{
+  state = {
+    persons: [
+      {name: "Max", age: 28},
+      {name: "Manu", age: 25},
+      {name: "Steph", age: 23}
+    ]
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1> First App huhu </h1>
+        <button> Switch Name </button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is racing</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
