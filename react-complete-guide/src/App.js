4f0725c48d21aa8f70bd704eb115dee80255ebf4
Priyansh Sangule <psangule@schoology.com>
2021-04-13 11:40:29 +0530

    Two way bindings

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,17 +7,44 @@ class App extends Component{
     persons: [
       {name: "Max", age: 28},
       {name: "Manu", age: 25},
-      {name: "Steph", age: 23}
+      {name: "Steph", age: 23},
+      {name: "Malark", age: 29}
     ]
+  }
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {name: "Max", age: 3},
+        {name: "Manu", age: 4},
+        {name: "Steph", age: 2},
+        {name: "Malark", age: 29}
+      ]
+    })
+  }
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: "Max", age: 3},
+        {name: "Manu", age: 4},
+        {name: "Steph", age: 2},
+        {name: event.target.value, age: 29}
+      ]
+    })
   }
 
   render(){
     return (
       <div className="App">
         <h1> First App huhu </h1>
-        <button> Switch Name </button>
+        <button onClick={this.switchNameHandler}> Switch Name </button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person 
+          name={this.state.persons[3].name} 
+          age={this.state.persons[3].age} 
+          changed={this.nameChangedHandler}
+        />
       </div>
     );
   }
